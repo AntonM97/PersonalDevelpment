5ff3206543b4fa9d9514290c145795129d15777b
AntonM97 <anton.matsibora@gmail.com>
2021-03-01 13:25:41 +0100

    3rd commit

--- src/training_scheduler/JavaTest.java
+++ src/training_scheduler/JavaTest.java
@@ -3,5 +3,6 @@ package training_scheduler;
 public class JavaTest {
 	public static void main(String[] args) {
 		System.out.println("Here is try nomber n");
+		System.out.println("check changes");
 	}
 }
